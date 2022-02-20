94a91ef5e01e6a4abef975b5e75ff98c84a7a7fe
Xwd <1324156920@qq.com>
2022-02-20 16:46:22 +0800

    my web page

---
+++ static/js/index.js
@@ -0,0 +1,19 @@
+// $("#academy_item").click(function(){
+// 	//移除原有的options
+// 	$("#academy_item option").empty();//清空子元素
+// 	//ajax请求-加载新的元素
+// 	$.ajax({
+// 		url:"http://localhost:8010/stasys_v2/get/academylist",
+// 		method:"POST",
+// 		async:true,
+// 		data:{},
+// 		dataType:"json",
+// 		success:function(result,status,xhr){
+// 			console.log(result))
+// 		}
+// 	})
+	
+// })
+
+// btn_modifyUserInfo
+// btn_saveUserInfo
