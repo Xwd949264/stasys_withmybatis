380156e426eca64af7954b3e1c7cf23cbd0b731c
Xwd <1324156920@qq.com>
2022-02-20 17:54:41 +0800

    相当于git commit -m '第一次提交项目文件'

---
+++ src/com/xwd/Student.java
@@ -0,0 +1,20 @@
+package com.xwd;
+
+/**
+ * @ClassName Student
+ * @Description: com.xwd
+ * @Auther: xiwd
+ * @Date: 2022/2/20 - 02 - 20 - 17:50
+ * @version: 1.0
+ */
+public class Student {
+    //properties
+
+    //setter
+
+    //getter
+
+    //constructors
+
+    //methods
+}
